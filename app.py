c0be87c489c673a78467544c8304071e65815fde
lailasalsabillah <lailasalsabillahoke@gmail.com>
2025-12-01 10:36:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Sistem Prediksi Kedalaman Gempa",
+    page_title="Prediksi Kedalaman Gempa",
     page_icon="🌋",
     layout="wide"
 )
 
 st.title("🌋 Sistem Prediksi Kedalaman Gempa Bumi")
 st.write("""
-Selamat datang di aplikasi **Prediksi Kedalaman Gempa** berbasis **LSTM & XGBoost**.
+Selamat datang di aplikasi **Prediksi Kedalaman Gempa Bumi** yang dibangun menggunakan model  
+**LSTM** dan **XGBoost**.
 
-Gunakan menu di sebelah kiri untuk:
-- 🔎 Melakukan prediksi
-- 📊 Melihat visualisasi dataset
-- 🗺️ Menampilkan peta lokasi gempa
-- 📥 Mengunduh hasil prediksi
+Gunakan menu Sidebar di kiri (📑 Pages) untuk:
+- ⚡ Melakukan Prediksi Kedalaman Gempa  
+- 📊 Melihat Visualisasi Data  
+- 🗺️ Melihat Peta Lokasi Gempa  
+- 📥 Mengunduh Hasil Prediksi  
+
+Aplikasi ini memanfaatkan dataset gempa 2020–2024 dan memprediksi kategori kedalaman:  
+**Shallow (<70 km)**, **Intermediate (70–300 km)**, dan **Deep (>300 km)**.
 """)
