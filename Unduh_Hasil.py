19398867779365b981b19869227a57bcbd4db498
lailasalsabillah <lailasalsabillahoke@gmail.com>
2025-12-01 10:24:10 +0700

    Create Unduh_Hasil.py

---
+++ Unduh_Hasil.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.title("📥 Unduh Dataset atau Hasil Prediksi")
+
+df = pd.read_csv("dataset/earthquake_dataset.csv")
+
+# Download dataset lengkap
+st.download_button(
+    "Download Dataset Gempa (CSV)",
+    df.to_csv(index=False),
+    "dataset_gempa.csv",
+    "text/csv"
+)
+
+st.info("Hasil prediksi juga dapat diunduh dari halaman Prediksi Gempa.")
