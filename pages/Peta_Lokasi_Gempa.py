5e66a7d7ddc7d293dfbe15fd9538076065c439a4
lailasalsabillah <lailasalsabillahoke@gmail.com>
2025-12-01 10:41:56 +0700

    Update Peta_Lokasi_Gempa.py

--- pages/Peta_Lokasi_Gempa.py
+++ pages/Peta_Lokasi_Gempa.py
@@ -5,16 +5,17 @@ from streamlit_folium import st_folium
 
 st.title("🗺️ Peta Lokasi Gempa")
 
-df = pd.read_csv("dataset/earthquake_dataset.csv")
+df = pd.read_csv("dataset_gempa.csv")
 
-m = folium.Map(location=[-2, 118], zoom_start=5)
+m = folium.Map(location=[-2, 120], zoom_start=5)
 
 for _, row in df.iterrows():
     folium.CircleMarker(
         location=[row["latitude"], row["longitude"]],
-        radius=5,
-        popup=f"M {row['mag']} | Depth {row['depth']} km",
-        color="red"
+        radius=3,
+        popup=f"Mag: {row['mag']} | Depth: {row['depth']} km",
+        color="red",
+        fill=True
     ).add_to(m)
 
-st_folium(m, width=800, height=480)
+st_folium(m, width=800)
